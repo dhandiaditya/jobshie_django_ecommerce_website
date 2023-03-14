285a9408810769c5c4fa069c311598f07d14ee35
dhandiaditya <86340419+dhandiaditya@users.noreply.github.com>
2023-03-14 13:18:53 +0530

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-import stripe
+#import stripe
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -9,13 +9,12 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
 from django.views.generic import ListView, DetailView, View
 
 from .forms import CheckoutForm, CouponForm, RefundForm, PaymentForm
 from .models import Item, OrderItem, Order, Address, Payment, Coupon, Refund, UserProfile
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
+#stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 def create_ref_code():
