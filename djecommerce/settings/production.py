1a2f290f2ba72976750dd873b87212ec218a12ba
dhandiaditya <86340419+dhandiaditya@users.noreply.github.com>
2023-03-14 15:13:11 +0530

    Update production.py

--- djecommerce/settings/production.py
+++ djecommerce/settings/production.py
@@ -17,7 +17,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
-        'PORT': ''
+        'PORT': '5432'
     }
 }
 ROOT_URLCONF = 'djecommerce.urls'
